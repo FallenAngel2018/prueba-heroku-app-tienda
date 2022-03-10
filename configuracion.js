3bdfa139f40351efe220adeb6ae8d43be948bc26
Isaac OBesso <isaacobesso2012@hotmail.com>
2022-03-10 02:24:32 -0500

    sss

--- configuracion.js
+++ configuracion.js
@@ -1,7 +1,7 @@
 const config = {
     // DB_URL: 'mongodb+srv://ups:ups2020@cluster0.pjyad.mongodb.net/tienda?retryWrites=true&w=majority',
     DB_URL: 'mongodb+srv://ups:ups2020@cluster0.5vdk8.mongodb.net/ups_examen?retryWrites=true&w=majority',
-    host: "https://pwp59-iobesso-mongodb-heroku.herokuapp.com",
+    //host: "https://pwp59-iobesso-mongodb-heroku.herokuapp.com",
     PUERTO: 3000
 }
 
